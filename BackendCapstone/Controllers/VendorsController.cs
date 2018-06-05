1ed68cf386ed4434b9b12b9481b09c32267c973c
Gina <ginamantonini@gmail.com>
2018-06-05 18:41:16 -0500

    new vendor can be added, verified with Postman

--- BackendCapstone/Controllers/VendorsController.cs
+++ BackendCapstone/Controllers/VendorsController.cs
@@ -20,5 +20,16 @@ namespace BackendCapstone.Controllers
 
             return Request.CreateResponse(HttpStatusCode.OK, result);
         }
+
+        [Route, HttpPost]
+        public HttpResponseMessage AddNewVendor(VendorsDto vendor)
+        {
+            var repository = new VendorsRepository();
+            var result = repository.Create(vendor);
+
+            if (result)
+                return Request.CreateResponse(HttpStatusCode.Created);
+            return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Vendor could not be added");
+        }
     }
 }
