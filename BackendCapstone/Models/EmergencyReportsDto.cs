3811534fdf9a50f1ab9cae033672165882891b3d
Gina <ginamantonini@gmail.com>
2018-06-06 16:55:30 -0500

    emergency reports can be added, tested with Postman

---
+++ BackendCapstone/Models/EmergencyReportsDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackendCapstone.Models
+{
+    public class EmergencyReportsDto
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int ManagerId { get; set; }
+        public string Caller { get; set; }
+        public string CallerPhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string IncidentDescription { get; set; }
+        public string ActionTaken { get; set; }
+        public int PropertyId { get; set; }
+    }
+}
---
+++ BackendCapstone/Services/EmergencyReportsRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using Dapper;
+using BackendCapstone.Models;
+
+namespace BackendCapstone.Services
+{
+    public class EmergencyReportsRepository
+    {
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(ConfigurationManager.ConnectionStrings["MetropolitanProperties"].ConnectionString);
+        }
+
+        public bool Create(EmergencyReportsDto emergencyreport)
+        {
+            using (var db = GetConnection())
+            {
+                db.Open();
+
+                var records = db.Execute(@"INSERT INTO [dbo].[EmergencyReport]
+                                                     ([Date]
+                                                     ,[ManagerId]
+                                                     ,[Caller]
+                                                     ,[CallerPhoneNumber]
+                                                     ,[Address]
+                                                     ,[IncidentDescription]
+                                                     ,[ActionTaken]
+                                                     ,[PropertyId])
+                                                VALUES
+                                                     (@Date
+                                                     ,@ManagerId
+                                                     ,@Caller
+                                                     ,@CallerPhoneNumber
+                                                     ,@Address
+                                                     ,@IncidentDescription
+                                                     ,@ActionTaken
+                                                     ,@PropertyId)", emergencyreport);
+                return records == 1;
+            }
+        }
+    }
+}
