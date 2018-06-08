42e1247aa006e59269cab21c56b50922624e07dd
Gina <ginamantonini@gmail.com>
2018-06-07 19:19:27 -0500

    Board members can be added, verified with Postman

---
+++ BackendCapstone/Models/BoardMembersDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackendCapstone.Models
+{
+    public class BoardMembersDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int BoardId { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ BackendCapstone/Services/BoardMembersRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using Dapper;
+using BackendCapstone.Models;
+
+namespace BackendCapstone.Services
+{
+    public class BoardMembersRepository
+    {
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(ConfigurationManager.ConnectionStrings["MetropolitanProperties"].ConnectionString);
+        }
+
+        public bool Create(BoardMembersDto boardmember)
+        {
+            using (var db = GetConnection())
+            {
+                db.Open();
+
+                var records = db.Execute(@"INSERT INTO [dbo].[BoardMember]
+                                                     ([Name]
+                                                     ,[BoardId]
+                                                     ,[PhoneNumber]
+                                                     ,[EmailAddress])
+                                                VALUES
+                                                     (@Name
+                                                     ,@BoardId
+                                                     ,@PhoneNumber
+                                                     ,@EmailAddress)", boardmember);
+                return records == 1;
+            }
+        }
+
+    }
+}
