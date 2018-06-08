844841d784064d1b1e69e3fa0c6e34bbe83f8379
Gina <ginamantonini@gmail.com>
2018-06-08 15:50:35 -0500

    new board can be added

---
+++ BackendCapstone/Models/BoardsDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackendCapstone.Models
+{
+    public class BoardsDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int PropertyId { get; set; }
+    }
+}
---
+++ BackendCapstone/Services/BoardsRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackendCapstone.Services
+{
+    public class BoardsRepository
+    {
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(ConfigurationManager.ConnectionStrings["MetropolitanProperties"].ConnectionString);
+        }
+
+        public bool Create(BoardsDto board)
+        {
+            using (var db = GetConnection())
+            {
+                db.Open();
+
+                var records = db.Execute(@"INSERT INTO [dbo].[Board]
+                                                     ([Name]
+                                                     ,[PropertyId])
+                                                VALUES
+                                                     (@Name
+                                                     ,@PropertyId)", board);
+                return records == 1;
+            }
+        }
+    }
+}
